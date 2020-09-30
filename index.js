5c9cf23cbf536c69d496a29b2aee151993d47e8d
Norathat Jongjitnant <norathat@gmail.com>
2020-09-30 11:22:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,16 +9,7 @@ function breakOut(array, changeValue, stopValue){
   return array
 }
 
-function keepGoing(array, changeValue, stopValue){
-  for (var i = 0; i <= array.length; i++) {
-    if (array[i] == stopValue) {
-      continue;
-    } else {
-      array[i] = changeValue
-    }
-  }
-  return array
-}
+
 
 function findBy(array, findFn){
   for (var i = 0; i < array.length; i++) {
